5e266817852f56a9215113f958d5811a89bd8d72
Stereokai <tom@stereokai.com>
2018-03-15 17:23:46 +0200

    Add capability to use AutoDll with multiple projects under one repo

--- src/createHash.js
+++ src/createHash.js
@@ -3,10 +3,11 @@ import crypto from 'crypto';
 const createHash = settings => {
   const hash = crypto.createHash('md5');
   const settingsJSON = JSON.stringify(settings);
+  const projectId = settings.project ? settings.project + '_' : '';
 
   hash.update(settingsJSON);
 
-  return [settings.env, settings.id, hash.digest('hex')].join('_');
+  return projectId + [settings.env, settings.id, hash.digest('hex')].join('_');
 };
 
 export default createHash;
